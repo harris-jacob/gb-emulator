71743cc4aff4ac4f4755fa6edb918691f8546932
harris-jacob <jacob.harris443@gmail.com>
2022-02-06 00:59:03 +0000

    removing stale yarn lock

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Debugger from './routes/debugger';
 import NotFoundPage from './routes/notfound';
 import GlobalStyle from './components/global-style';
@@ -8,12 +8,12 @@ const App: React.FC = () => {
     return (
         <div id="root">
             <GlobalStyle />
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                 <Route path="/" element={<Debugger/>} />
                 <Route path="*" element={<NotFoundPage/>} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 };
